580bc4b3e527f62b2c448bcd23291c4ffb153e22
peterclermy <peterclermy232@gmail.com>
2022-07-26 08:20:02 +0300

    import mainNavigation

--- .history/src/layout/Layout_20220726081902.js
+++ .history/src/layout/Layout_20220726081902.js
@@ -4,7 +4,7 @@ import MainNavigation from './MainNavigation'
 
 function Layout(props) {
     return (
-        <div cll>
+     //   <div className="main">
            <MainNavigation /> 
            <main>
             {props.children}
